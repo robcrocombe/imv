15c8f9e0e71339d99b1f1d4f8cbd6ad86171b0a8
Rob Crocombe <rjwc1993@yahoo.co.uk>
2019-10-23 23:51:10 +0100

    Use describe to group tests

--- tests/imv.spec.ts
+++ tests/imv.spec.ts
@@ -36,103 +36,109 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-it('moves a single file', async () => {
-  setEdits(['./foo/fidget2.txt']);
+describe('Basic functionality', () => {
+  it('moves a single file', async () => {
+    setEdits(['./foo/fidget2.txt']);
 
-  await run(['./foo/fidget.txt'], { editor }, true);
+    await run(['./foo/fidget.txt'], { editor }, true);
 
-  expect(mockedFs.__getFile('./foo/fidget.txt')).toBeNull();
-  expect(mockedFs.__getFile('./foo/fidget2.txt')).toBe('weapon');
+    expect(mockedFs.__getFile('./foo/fidget.txt')).toBeNull();
+    expect(mockedFs.__getFile('./foo/fidget2.txt')).toBe('weapon');
 
-  expect(log).toHaveBeenCalledTimes(1);
+    expect(log).toHaveBeenCalledTimes(1);
+  });
+
+  it('moves using a glob pattern', async () => {
+    setEdits(['./flag2.png', './bar2/opera.png', './foo/myth.jpg']);
+
+    await run(['./**/*.png'], { editor }, true);
+
+    expect(mockedFs.__getFile('./flag.png')).toBeNull();
+    expect(mockedFs.__getFile('./flag2.png')).toBe('island');
+
+    expect(mockedFs.__getFile('./bar/opera.png')).toBeNull();
+    expect(mockedFs.__getFile('./bar2/opera.png')).toBe('pump');
+
+    expect(mockedFs.__getFile('./foo/myth.png')).toBeNull();
+    expect(mockedFs.__getFile('./foo/myth.jpg')).toBe('tram');
+
+    expect(log).toHaveBeenCalledTimes(1);
+  });
 });
 
-it('moves using a glob pattern', async () => {
-  setEdits(['./flag2.png', './bar2/opera.png', './foo/myth.jpg']);
+describe('Overwrite behaviour', () => {
+  it('cannot overwrite matching files with overwrite=false', async () => {
+    setEdits(['./foo/brand_new.js', './foo/skate.js', './foo/guitar.js']);
 
-  await run(['./**/*.png'], { editor }, true);
+    await run(
+      ['./foo/guitar.js', './foo/skate.js', './foo/dollar.js'],
+      { editor, overwrite: false },
+      false
+    );
 
-  expect(mockedFs.__getFile('./flag.png')).toBeNull();
-  expect(mockedFs.__getFile('./flag2.png')).toBe('island');
+    expect(log.error).toHaveBeenCalledTimes(1);
+    expect(log.error).toHaveBeenCalledWith('Error: file ./foo/guitar.js already exists.');
+  });
 
-  expect(mockedFs.__getFile('./bar/opera.png')).toBeNull();
-  expect(mockedFs.__getFile('./bar2/opera.png')).toBe('pump');
+  it('cannot overwrite matching files with overwrite=true', async () => {
+    setEdits(['./foo/brand_new.js', './foo/skate.js', './foo/guitar.js']);
 
-  expect(mockedFs.__getFile('./foo/myth.png')).toBeNull();
-  expect(mockedFs.__getFile('./foo/myth.jpg')).toBe('tram');
+    await run(
+      ['./foo/guitar.js', './foo/skate.js', './foo/dollar.js'],
+      { editor, overwrite: true },
+      false
+    );
 
-  expect(log).toHaveBeenCalledTimes(1);
+    expect(log.error).toHaveBeenCalledTimes(1);
+    expect(log.error).toHaveBeenCalledWith(
+      'Error: cannot rename ./foo/dollar.js to ./foo/guitar.js because the new file is also pending movement.'
+    );
+  });
+
+  it('cannot overwrite non-matching files with overwrite=false', async () => {
+    setEdits(['./foo/guitar.js']);
+
+    await run(['./foo/fidget.txt'], { editor, overwrite: false }, false);
+
+    expect(log.error).toHaveBeenCalledTimes(1);
+    expect(log.error).toHaveBeenCalledWith('Error: file ./foo/guitar.js already exists.');
+  });
+
+  it('overwrites non-matching files with overwrite=true', async () => {
+    setEdits(['./foo/guitar.js']);
+
+    await run(['./foo/fidget.txt'], { editor, overwrite: true }, true);
+
+    expect(log).toHaveBeenCalledTimes(1);
+  });
+
+  // , async () => {}
+  it.todo('sends non-matching files to the recycle bin with trash=true');
+
+  it('cannot run with both overwrite=true and trash=true', async () => {
+    await run(['./foo/fidget.txt'], { editor, overwrite: true, trash: true }, false);
+
+    expect(log.error).toHaveBeenCalledTimes(1);
+    expect(log.error).toHaveBeenCalledWith(
+      'Please use either `overwrite` or `trash` options, but not both at the same time.'
+    );
+  });
 });
 
-it('cannot overwrite matching files with overwrite=false', async () => {
-  setEdits(['./foo/brand_new.js', './foo/skate.js', './foo/guitar.js']);
+describe('Missing input', () => {
+  it('cannot run without input', async () => {
+    await run([''], { editor }, true);
 
-  await run(
-    ['./foo/guitar.js', './foo/skate.js', './foo/dollar.js'],
-    { editor, overwrite: false },
-    false
-  );
+    expect(log.warn).toHaveBeenCalledTimes(1);
+    expect(log.warn).toHaveBeenCalledWith('No files found matching "". Aborting.');
+  });
 
-  expect(log.error).toHaveBeenCalledTimes(1);
-  expect(log.error).toHaveBeenCalledWith('Error: file ./foo/guitar.js already exists.');
-});
+  it('cannot run without a matching glob pattern', async () => {
+    await run(['./**/*.psd'], { editor }, true);
 
-it('cannot overwrite matching files with overwrite=true', async () => {
-  setEdits(['./foo/brand_new.js', './foo/skate.js', './foo/guitar.js']);
-
-  await run(
-    ['./foo/guitar.js', './foo/skate.js', './foo/dollar.js'],
-    { editor, overwrite: true },
-    false
-  );
-
-  expect(log.error).toHaveBeenCalledTimes(1);
-  expect(log.error).toHaveBeenCalledWith(
-    'Error: cannot rename ./foo/dollar.js to ./foo/guitar.js because the new file is also pending movement.'
-  );
-});
-
-it('cannot overwrite non-matching files with overwrite=false', async () => {
-  setEdits(['./foo/guitar.js']);
-
-  await run(['./foo/fidget.txt'], { editor, overwrite: false }, false);
-
-  expect(log.error).toHaveBeenCalledTimes(1);
-  expect(log.error).toHaveBeenCalledWith('Error: file ./foo/guitar.js already exists.');
-});
-
-it('overwrites non-matching files with overwrite=true', async () => {
-  setEdits(['./foo/guitar.js']);
-
-  await run(['./foo/fidget.txt'], { editor, overwrite: true }, true);
-
-  expect(log).toHaveBeenCalledTimes(1);
-});
-
-// , async () => {}
-it.todo('sends non-matching files to the recycle bin with trash=true');
-
-it('cannot execute with both overwrite=true and trash=true', async () => {
-  await run(['./foo/fidget.txt'], { editor, overwrite: true, trash: true }, false);
-
-  expect(log.error).toHaveBeenCalledTimes(1);
-  expect(log.error).toHaveBeenCalledWith(
-    'Please use either `overwrite` or `trash` options, but not both at the same time.'
-  );
-});
-
-it('cannot execute without input', async () => {
-  await run([''], { editor }, true);
-
-  expect(log.warn).toHaveBeenCalledTimes(1);
-  expect(log.warn).toHaveBeenCalledWith('No files found matching "". Aborting.');
-});
-
-it('cannot execute without a matching glob pattern', async () => {
-  await run(['./**/*.psd'], { editor }, true);
-
-  expect(log.warn).toHaveBeenCalledTimes(1);
-  expect(log.warn).toHaveBeenCalledWith('No files found matching "./**/*.psd". Aborting.');
+    expect(log.warn).toHaveBeenCalledTimes(1);
+    expect(log.warn).toHaveBeenCalledWith('No files found matching "./**/*.psd". Aborting.');
+  });
 });
 
 function setEdits(arr: string[]) {
